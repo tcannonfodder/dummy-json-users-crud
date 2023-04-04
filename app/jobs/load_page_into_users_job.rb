be45184a0d845a51bb353cca8b65ab9dcdd940b3
Thomas Cannon <tcannon00@gmail.com>
2023-04-04 16:04:36 -0400

    WIP: `LoadPageIntoUsersJob`, loads `User` data from DummyJSON
    * The job requires a `page` number and then:
    	* Calls `DummyUsersClient.load` for the given page
    	* Tries to create a `User` for each JSON object in the response
    		* Skips any records that already exist to prevent overwriting data
    	* If the current result was not empty, there are likely still records
    		and we should enqueue the next page
    * The `per_page` and automatically enqueueing the next page as a job
    	are both named parameters that can be overridden when the job is
    	called

---
+++ app/jobs/load_page_into_users_job.rb
@@ -0,0 +1,27 @@
+class LoadPageIntoUsersJob < ApplicationJob
+  queue_as :default
+
+  def perform(page: , per_page: 50, enqueue_next: true)
+    page_data = DummyUsersClient.load(page: page, per_page: per_page)
+    users = page_data["users"]
+    users.each do |user_json|
+      next if User.find_by(dummy_json_id: user_json["id"])
+
+      User.create!({
+        dummy_json_id: user_json["id"],
+        first_name: user_json["firstName"],
+        middle_name: user_json["middleName"],
+        last_name: user_json["lastName"],
+        email: user_json["email"],
+        domain: user_json["domain"],
+        ip_address: user_json["ip"],
+        mac_address: user_json["macAddress"],
+        user_agent: user_json["userAgent"]
+      })
+    end
+
+    if enqueue_next && !users.empty?
+      LoadPageIntoUsersJob.perform_later(page: page + 1, per_page: per_page)
+    end
+  end
+end
---
+++ test/jobs/load_page_into_users_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LoadPageIntoUsersJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
