9ffdc0f20025f18ed3d6a2ecffcd29aee4d30584
Thomas Cannon <tcannon00@gmail.com>
2023-04-05 09:00:59 -0400

    Add `PaginationCalculator`: PORO for pagination logic
    * Provides a standard implementation for determining:
    	* If there's a next page after the current page
    	* If there's a previous page behind the current page
    	* What the current `offset` is for a page of data, and what the
    		pagination offset should be for a query
    	* The last page of a query
    	* The previous page (if possible)

---
+++ app/lib/pagination_calculator.rb
@@ -0,0 +1,41 @@
+class PaginationCalculator
+  attr_accessor :current_page, :per_page, :total_items, :current_item_count
+
+  def initialize(current_page:, per_page:, total_items:, current_item_count:)
+    self.current_page = current_page
+    self.per_page = per_page
+    self.total_items = total_items
+    self.current_item_count
+  end
+
+  def self.pagination_offset(page:, per_page:)
+    return 0 if page <= 0
+    return (page - 1) * per_page
+  end
+
+  def has_next_page?
+    current_offset < total_items
+  end
+
+  def has_previous_page?
+    page < 2
+  end
+
+  def next_page
+    return nil unless has_next_page?
+    current_page + 1
+  end
+
+  def previous_page
+    return nil unless has_previous_page?
+    current_page - 1
+  end
+
+  def last_page_number
+    self.total_items.fdiv(per_page).ceil
+  end
+
+  def current_offset
+    ((current_page - 1) * per_page) + current_item_count
+  end
+end
