b79bd7c055266ffff449feac81c72905ed36d6d4
Thomas Cannon <tcannon00@gmail.com>
2023-04-05 11:11:39 -0400

    Add `user_status` helper to render emoji badge for user's `status`

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,17 @@ module UsersHelper
   def domain_url(domain)
     URI::HTTP.build(host: domain).to_s
   end
+
+  def user_status(user)
+    case user.status.to_sym
+    when :unknown
+      return "❓"
+    when :warn
+      return "⚠️"
+    when :alert
+      return "🚨"
+    when :approved
+      return "✅"
+    end
+  end
 end
