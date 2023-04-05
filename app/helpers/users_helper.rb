2322d86696faa64b69567e5ee9d113d469657d0a
Thomas Cannon <tcannon00@gmail.com>
2023-04-05 09:27:24 -0400

    Add `domain_url` to build an HTTP URL for a given domain name

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def user_name(user)
     return [user.first_name, user.last_name].join(' ')
   end
+
+  def domain_url(domain)
+    URI::HTTP.build(host: domain).to_s
+  end
 end
