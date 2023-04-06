c506aff74b289c7f05a95295ee60fc0422f1f156
Thomas Cannon <tcannon00@gmail.com>
2023-04-05 21:17:05 -0400

    Add `FilterAndSearch.filter` method to chain together filters
    * Accepts an ActiveRecord `scope`, which it chains additional
    	`where` filters on based on the values of the `FilterAndSearch`
    	instance
    	* This allows us to define the filtering & searching logic in a
    		decoupled way from the actual controller code, and extend it
    		as needed

--- app/lib/filter_and_search.rb
+++ app/lib/filter_and_search.rb
@@ -2,4 +2,26 @@ class FilterAndSearch
   include ActiveModel::API
 
   attr_accessor :status, :note_presence, :note, :email, :name
+
+  def filter(scope:)
+    if status.present?
+      scope = scope.where(status: status)
+    end
+
+    if note_presence
+      scope = scope.where("note IS NOT NULL")
+    elsif note.present?
+      scope = scope.where("LOWER(note) LIKE LOWER(?)", "%#{ApplicationRecord.sanitize_sql_like(note)}%")
+    end
+
+    if email.present?
+      scope = scope.where("LOWER(email) LIKE LOWER(?)", "%#{ApplicationRecord.sanitize_sql_like(email)}%")
+    end
+
+    if name.present?
+      scope = scope.where("LOWER(name) LIKE LOWER(?)", "%#{ApplicationRecord.sanitize_sql_like(name)}%")
+    end
+
+    return scope
+  end
 end
